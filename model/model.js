6031027d68621f5a68ed9f50c9e018e5ab9e6742
Claratruelsen <clara.l.truelsen@gmail.com>
2021-04-02 12:56:23 +0200

    opdateret HTML sider samt dannet os et overblik over klasserne

--- model/model.js
+++ model/model.js
@@ -1,90 +1,90 @@
-class System{
-    constructor(activeUsers){
-        this._activeUsers = activeUsers;
+class User{
+    constructor(userId, email, password, fullName, DOB, region, gender, preferredGender, minAge, maxAge, img, bio, matches){
+        this.userId = userId;
+        this._email = email;
+        this._password = password
+        this.fullName = fullName;
+        this.DOB = DOB;
+        this.region = region;
+        this.gender = gender;
+        this.preferredGender = preferredGender;
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+        this.bio = bio;
+        this.matches = matches; 
+        this.img = img;
     }
 
     login(){
+        // valider - lav API kald
+        // gem i local storage så man forbliver logget ind
+        // redirect til profil side
+            }
+        
+    logout(){
+        // "fjern" fra local storage 
+        //tilbage til startside
+            }
 
+    updateProfile(){
+        // sender videre til anden html side hvor man kan redigere alle oplysninger
+        // herunder en slet bruger knap
+    }
+
+    updateBtn(){
+        //knap der opdaterer alle felterne i formen - hvis de ikke er fyldt ud så beholdes de gamle
+    }
+
+    getMatches(){
+        // ny HTML side med liste over matches 
+        //fjern match
+        //chat med match - åbner ny side med chat
+    }
+
+    like(){
+        // hvis begge har liket skal en pop up besked komme OG profilen skal tilføjes til matchliste
+    }
+
+    dislike(){
+        //viser ny foreslået match
+    }
+}
+
+
+
+class Admin {
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+    login(){
+        // kommer ind på ny HTML side med 
     }
 
     logout(){
 
     }
 
-}
+    //
 
-class Test{
-    
-}
 
-class User{
-    constructor(userId, username, password, firstName, lastName, age, gender, sexuality, interests, address, matches, img){
-        this.userId = userId;
-        this._username = username;
-        this._password = password
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullName = function(){
-            return this.firstName + " " + this.lastName
-        }
-        this.age = age;
-        this.gender = gender;
-        this.sexuality = sexuality;
-        this.interests = interests;
-        this._address = address;
-        this.matches = matches; 
-        this.img = img;
+    adminUpdateUser(){
 
     }
 
-    getInfo(){
-    }
 
-    updateProfile(){
 
-    }
-
-    getMatches(){
-
-    }
-
-    likeUser(){
-
-    }
 }
 
 
 
-class Interests{
-    constructor(interests){
-        this.interests = interests;
 
-    }
 
-    addInterests(){
 
-    }
 
-    deleteInterests(){
 
-    }
-
-    getInterest(){
-
-    }
-}
-
-class Address{
-    constructor(city, region, country){
-        this.city = city;
-        this.region = region;
-        this.country = country;
-    }
-
-    compareAddress(){
-
-    }
-}
+//ved ikke om disse skal bruges endnu
 
 class Img {
     constructor(path, mimeType){
@@ -106,17 +106,15 @@ class Img {
 }
 
 class Match{
-    constructor(matchId, user1, user2, chat, commonInterests, address){
+    constructor(matchId, user1, user2, chat){
         this._matchId = matchId;
         this.user1 = user1;
         this.user2 = user2;
         this.chat = chat;
-        this._commonInterests = commonInterests; 
-        this._address = address
     }
 
     //funktioner
-    unMatch(){
+    deleteMatch(){
     }
 
     openChat(){
