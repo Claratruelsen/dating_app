49ec560dcaa64889dafe505b92fa5d846649d2b2
Claratruelsen <clara.l.truelsen@gmail.com>
2021-04-13 15:18:56 +0200

    login

--- view/login.js
+++ view/login.js
@@ -1,12 +1,9 @@
+var loginBtn = document.getElementById("login")
 
-
-
-var getButton = document.getElementById("getUsers")
-
-getButton.addEventListener("click", function(){
-
+loginBtn.addEventListener("click", function(){
     var email = document.getElementById("email").value
-    fetch(`http://localhost:7071/api/user?name=${name1}`)
+    var password = document.getElementById("hashed_password").value
+    fetch(`http://localhost:7071/api/getUser?email=${email}?hashed_password=${password}`)
         .then(
             function(response){
                 if (response.status !== 200){
