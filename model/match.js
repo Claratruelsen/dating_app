67c0e38a1a9a00ef7c9b06bd84e533cf88c58e14
claramille <cm.kaloe@hotmail.com>
2021-05-07 08:56:16 +0200

    flere rettelseer

--- model/match.js
+++ model/match.js
@@ -83,12 +83,6 @@ dislikebtn.addEventListener("click", () => {
     
         //skal matche alle brugere indefor samme region - skal lave kald til DB som sorterer så der laves en liste over potentielle matches - dem som bor i samme area - tager bare oppe fra og ned
         //Altså denne matching algortime er jo reelt set lavet i system, men jeg ved bare ikke hvor jeg skal placere den
-<<<<<<< HEAD
-    }
-
-    
-=======
->>>>>>> c11f7f2a035f60e383284416b62b2db4f53fca86
 
 /*
     like(){
